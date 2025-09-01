4fc219aada135021b443e1a974e4b96a934bffce
lowlowCodee <laurenceg079@gmail.com>
2025-09-01 11:34:19 +0800

    Update

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 def checkMenu():
     
     print("MENU")
-    print("1. Espresso  == ₱65")
-    print("2. Americano == ₱75")
-    print("3. Latte     == ₱55")
+    print("====================")
+    print("1. Espresso  == ₱65 |")
+    print("2. Americano == ₱75 |")
+    print("3. Latte     == ₱55 |")
+    print("====================")
+
+def order():
+    print("MENU")
+    print("====================")
+    print("1. Espresso  == ₱65 |")
+    print("2. Americano == ₱75 |")
+    print("3. Latte     == ₱55 |")
+    print("====================")
+    order = int(input("Enter your Order: "))
 
 print("Welcome, Customers")
 print("---Coffee Yow!----")
 
-
+print("=================")
 print("1. Check Menu")
+print("2. Order")
+print("=================")
 choice = int(input("Enter your choice: "))
 
 if choice == 1:
     checkMenu()
+elif choice == 2:
+    order()
 
 
 
