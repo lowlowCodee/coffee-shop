2771b499fd90f5775d496462467175cd00e3ef82
lowlowCodee <laurenceg079@gmail.com>
2025-09-01 11:21:10 +0800

    update

---
+++ main.py
@@ -0,0 +1,22 @@
+def checkMenu():
+    
+    print("MENU")
+    print("1. Espresso  == ₱65")
+    print("2. Americano == ₱75")
+    print("3. Latte     == ₱55")
+
+print("Welcome, Customers")
+print("---Coffee Yow!----")
+
+
+print("1. Check Menu")
+choice = int(input("Enter your choice: "))
+
+if choice == 1:
+    checkMenu()
+
+
+
+
+
+
